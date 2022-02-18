245fce4944287c11f9ee0c77375a7c45a9be86d6
Brent <brentvdwijdeven@gmail.com>
2022-02-18 11:09:48 +0100

    Improve main.py

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
 from extract_data import download_and_unzip_twitter_dataset
-from transformations import *
+from transformations import read_csv_to_spark_df, drop_columns_and_rows, train_test_split, load_or_fit_model_pipeline, evaluate_pipeline_performance
+import timeit
+
+
+# determine script runtime
+start_time = timeit.default_timer()
 
 
 data_destination_folder = 'data/twitter_sentiment/'
 file_name = 'twitter_data2.csv'
+#
+# from pyspark import SparkContext
+# from pyspark.sql import SparkSession
+# """ De initialize moet mogelijk naar main.py"""
+# # Initialize spark session
+# # sc = SparkContext.getOrCreate()
+# spark = SparkSession.builder.master('local[*]').appName('twitter_sentiment_analysis').config("spark.ui.port", "4040")\
+#     .getOrCreate()
+# # access spark web ui by visiting http://localhost:4040/jobs/ . This is only accessible while script is running!
+
 
 # call download and unzip function for twitter dataset
 download_and_unzip_twitter_dataset(data_file_name=file_name, destination_folder=data_destination_folder)
 
+# create spark dataframe from downloaded csv
+my_data = read_csv_to_spark_df(data_destination_folder, file_name)
 
-read_csv_to_spark_df(data_destination_folder, file_name)
+# drop columns and rows
+my_data = drop_columns_and_rows(df=my_data, columns_to_drop=['date', 'flag'])
+
+# train test split
+(train_set, val_set, test_set) = train_test_split(df=my_data , split_percentages=[0.95, 0.025, 0.025])
+print(train_set.show())
+print('hallee')
+
+# load model pipeline if it is saved otherwise fit model and save it
+pipeline_fitted, train_df_fitted = load_or_fit_model_pipeline(pipeline_path='model/lr_model_pipeline', model_path='model/lr_model_trained', train_set=train_set)
+
+
+accuracy, roc_auc, predictions_dataset = evaluate_pipeline_performance(test_set=test_set, pipeline_fitted=pipeline_fitted)
+
+
+end_time = timeit.default_timer()
+
+print(end_time - start_time)
+
--- transformations.py
+++ transformations.py
@@ -1,25 +1,21 @@
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 import pyspark.sql.types as tp
-import timeit
 import os
 
 # prediction imports
 from pyspark.ml.feature import CountVectorizer
 from pyspark.ml.classification import LogisticRegression
-from pyspark.ml.feature import  IDF, Tokenizer
+from pyspark.ml.feature import IDF, Tokenizer
 from pyspark.ml.feature import StringIndexer
-from pyspark.ml import Pipeline, PipelineModel # ik begrijp het verschil hiertussen nog niet
+from pyspark.ml import Pipeline, PipelineModel  # ik begrijp het verschil hiertussen nog niet
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
-
-# determine script runtime
-start_time = timeit.default_timer()
 
 
 """ De initialize moet mogelijk naar main.py"""
 # Initialize spark session
 sc = SparkContext.getOrCreate()
-spark = SparkSession.builder.master('local[*]').appName('twitter_sentiment_analysis').config("spark.ui.port","4040")\
+spark = SparkSession.builder.master('local[*]').appName('twitter_sentiment_analysis').config("spark.ui.port", "4040")\
     .getOrCreate()
 # access spark web ui by visiting http://localhost:4040/jobs/ . This is only accessible while script is running!
 
@@ -43,10 +39,7 @@ def read_csv_to_spark_df(data_path, file_name):
     return spark_df
 
 
-my_data = read_csv_to_spark_df('data/twitter_sentiment/', 'twitter_data.csv')
-
-
-def drop_columns_and_rows(df, columns_to_drop): # can add type of df here in function argument
+def drop_columns_and_rows(df, columns_to_drop):  # can add type of df here in function argument
 
     # drop columns
     df = df.drop(*columns_to_drop)
@@ -57,31 +50,21 @@ def drop_columns_and_rows(df, columns_to_drop): # can add type of df here in fun
     return df
 
 
-my_data = drop_columns_and_rows(df=my_data, columns_to_drop=['date', 'flag'])
-
-
 def train_test_split(df, split_percentages):
-    # split_percentages is list with three percentages: train, valid, test
 
+    # split_percentages is list with three percentages: train, valid, test
     (train_set, val_set, test_set) = df.randomSplit(split_percentages, seed=2000)
 
     # hier nog ff testen of de dubbele () niet teveel is
-    return (train_set, val_set, test_set)
-
-# train test split
-
-
-(train_set, val_set, test_set) = train_test_split(df=my_data , split_percentages=[0.95, 0.025, 0.025])
-print(train_set.show())
-print('hallee')
-
+    return train_set, val_set, test_set
 
 
 # some kind of prediction
 """
 https://stackoverflow.com/questions/63703994/spark-v3-0-0-warn-dagscheduler-broadcasting-large-task-binary-with-size-xx
 reducing task size => reduce the data its handling
-First, check number of partitions in dataframe via df.rdd.getNumPartitions() After, increase partitions: df.repartition(100)
+First, check number of partitions in dataframe via df.rdd.getNumPartitions() After, increase partitions: 
+df.repartition(100)
 """
 
 # evt toevoegen: parameter tuning logistic regression, text cleaning, confusion matrix evaluation
@@ -95,6 +78,7 @@ def save_pipeline(pipeline_model, path):
     pipeline_model.write().overwrite().save(path)
 
     return print('Pipeline saved to {}'.format(path))
+
 
 def create_classification_pipeline():
     """ Deze functie met meer arguments opzetten zodat hij niet zo fixed is."""
@@ -113,11 +97,7 @@ def create_classification_pipeline():
     # Establish pipeline based on the steps defined before
     pipeline = Pipeline(stages=[tokenizer, cv, idf, label_stringIdx, lr])
 
-
     return pipeline
-
-
-
 
 
 def fit_lr_model_pipeline(pipeline_model, pipeline_path, model_path, train_set):
@@ -137,11 +117,7 @@ def fit_lr_model_pipeline(pipeline_model, pipeline_path, model_path, train_set):
     print(lr_model)
     lr_model.write().overwrite().save(model_path)
 
-
     return fit_pipeline, train_df_fitted
-
-
-
 
 
 def load_or_fit_model_pipeline(pipeline_path, model_path, train_set):
@@ -162,12 +138,6 @@ def load_or_fit_model_pipeline(pipeline_path, model_path, train_set):
     return pipelineFit, train_df_fitted
 
 
-pipeline_fitted, train_df_fitted = load_or_fit_model_pipeline(pipeline_path='model/lr_model_pipeline', model_path='model/lr_model_trained', train_set=train_set)
-
-
-
-
-
 # for more universal saving and loading refer to https://stackoverflow.com/questions/57038445/load-model-pyspark
 # WHEN TO USE A VECTOR ASSEMBLER TO CREATE A SINGLE COLUMN WITH ALL FEATURES?
 
@@ -177,12 +147,12 @@ def evaluate_pipeline_performance(test_set, pipeline_fitted):
     # transform validation dataset (or test dataset
     predictions_dataset = pipeline_fitted.transform(test_set)
 
-
     # evaluate function
     evaluator = BinaryClassificationEvaluator(rawPredictionCol="rawPrediction")
 
     # accuracy
-    accuracy = predictions_dataset.filter(predictions_dataset.label == predictions_dataset.prediction).count() / float(val_set.count())
+    accuracy = predictions_dataset.filter(predictions_dataset.label == predictions_dataset.prediction).count() \
+        / float(test_set.count())
 
     # ROC AUC
     roc_auc = evaluator.evaluate(predictions_dataset)
@@ -194,14 +164,3 @@ def evaluate_pipeline_performance(test_set, pipeline_fitted):
     print("ROC-AUC: {0:.4f}".format(roc_auc))
 
     return accuracy, roc_auc, predictions_dataset
-
-accuracy, roc_auc, predictions_dataset = evaluate_pipeline_performance(test_set=test_set, pipeline_fitted=pipeline_fitted)
-
-# save predictions as well
-# predictions_dataset.toPandas().to_csv('results/predictions.csv') # er is niet veel heil in het opslaan van dit
-end_time = timeit.default_timer()
-
-print(end_time - start_time)
-
-
-
